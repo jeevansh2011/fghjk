a09e427ace9d8b17b0725f605978b31f81e81d53
jeevansh2011 <90452280+jeevansh2011@users.noreply.github.com>
2023-07-22 00:00:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+function setup(){
+canvas=createCanvas(600,400);
+canvas.position(110,250);
+
+video=createCapture(VIDEO);
+video.hide();
+tint_color="";
+}
+
+function draw (){
+circle(30, 30, 20);
+describe('A white circle with black outline in the middle of a gray canvas.');
+}
+
